c49d10388434e27d0f72fb9dd5fa97e229868378
Isaias de Jesus Barros <isaiasjbarross@gmail.com>
2022-10-05 14:46:05 -0400

    Praticando com matriz

--- Matrizes/PraticandoComMatriz/Program.cs
+++ Matrizes/PraticandoComMatriz/Program.cs
@@ -4,7 +4,7 @@ namespace PraticandoComMatriz {
     internal class Program {
         static void Main(string[] args) {
 
-            int[,] matriz = new int[2, 3];
+            //int[,] matriz = new int[2, 2];
 
             // matriz.GetLength(0) obtém o tamanho de elementos da linha;
             // matriz.GetLength(1) obtém o tamanho de elementos da coluna
@@ -14,11 +14,36 @@ namespace PraticandoComMatriz {
             //      Console.Write($"Digite [{i + 1}, {j + 1}]: ");
             //        matriz[i, j] = int.Parse(Console.ReadLine());
 
-            //    }  
-            //}
-            
             /*
-    
+             14) Escreva um programa que leia uma matriz de ordem 5 e verifique se os
+                 elementos da diagonal principal (da esquerda para a direita)
+                são os mesmos da diagonal 
+                secundária (direita pra esquerda).
+             */
+
+            int[,] matrizB = new int[4, 4];
+
+            for (int i = 0; i < 4; i++) {
+
+                for (int j = 0; j < 4; j++) {
+                    Console.Write($"Digite [{i},{j}]: ");
+                    matrizB[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            for (int i = 0; i < 4; i++) {
+                for (int j = 0; j < 4; j++) {
+
+                    Console.Write($"[{matrizB[i, j]}]");
+                    if (i + j == 2) {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.Write($"[{matrizB[i, j]}]");
+                        Console.ResetColor();
+                    }
+                   
+                }
+                Console.WriteLine();
+            }
             Console.ReadKey();
         }
     }
