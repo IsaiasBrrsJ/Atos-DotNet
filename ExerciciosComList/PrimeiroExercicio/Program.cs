2d601ff982721aa95e05d55ffed03d34ae09463f
Isaias de Jesus Barros <isaiasjbarross@gmail.com>
2022-10-10 13:23:49 -0400

    Exercicios com list

---
+++ ExerciciosComList/PrimeiroExercicio/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace PrimeiroExercicio {
+    internal class Program {
+        static void Main(string[] args) {
+            /*
+             
+             1. Fazer um programa em VS que popule uma lista de nomes, com nomes completos obrigatoriamente.
+                Ao cadastrar o nome na lista, o nome deve ser validado para que tenha no mínimo duas palavras e 
+                que não esteja na lista. Ao final, caso o nome não esteja na lista, cadastra-lo em maiúsculo e 
+                exibir a lista ordenada
+             */
+
+            string nome = "";
+            string[] nomeSplit;
+
+            List<string> nomePessoas = new List<string>();
+            Console.WriteLine("Digite 1 para sair\n");
+            do {
+               
+                Console.Write("Informe seu nome completo: ");
+                 nome = Console.ReadLine();
+                 nomeSplit = nome.Split(" ");
+
+                if (nome == "1") {
+                    break;
+                }
+                else {
+
+
+                    if (nomeSplit.Length < 2) {
+                        Console.WriteLine("Nome precisa ser completo");
+                    }
+                    else if (nomePessoas.Contains(nome.ToUpper())) {
+                        Console.WriteLine("Nome informado já cadastrado");
+                    }
+                    else {
+                        nomePessoas.Add(nome.ToUpper());
+                    }
+
+                }
+            
+            } while (true);
+
+            nomePessoas.Sort();
+
+            if (nomePessoas.Count > 0) {
+
+                Console.WriteLine("Exibindo nomes");
+                foreach (var nomes in nomePessoas) {
+                    Console.WriteLine(nomes);
+                }
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ExerciciosComList/SegundoExercicio/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace SegundoExercicio {
+    internal class Program {
+        static void Main(string[] args) {
+
+            /*
+             2. Fazer um programa em VS que popule uma lista com números inteiros (0 a 1000) que sejam sorteados
+                randomicamente. O programa deve pedir ao usuário quantos números deseja armazenar. 
+                Ao final, o programa
+                deve exibir os números populados na lista.
+             
+             */
+
+
+            int qtdQueDesejaArmazenar = 0;
+            List<int> numerosArmazenados = new List<int>();
+
+            Console.WriteLine("Informe a quantidade de números aleátorios que deseja armazenas na lista: ");
+             qtdQueDesejaArmazenar = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < qtdQueDesejaArmazenar; i++) {
+
+                numerosArmazenados.Add(new Random().Next(0, 1001));
+            }
+
+            Console.WriteLine("Números que foram armazenados na lista\n");
+            foreach (var numeros in numerosArmazenados) {
+                Console.WriteLine(numeros);
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
