1b36edbc1cdc73d694f89f0577bc24a731ce51bc
Isaias de Jesus Barros <isaiasjbarross@gmail.com>
2022-10-13 14:57:09 -0400

    Exemplo de interface

---
+++ OrientaçãoaObjetos/Interface/SegundoOO/IEletronico.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SegundoOO
+{
+    interface Eletronico
+    {
+        //interface não pode conter atributos;
+        //private int _volts;
+        //private int _potencia;
+        //private bool _ligado;
+
+        public void Ligar();
+        public void Desligar();
+
+    }
+}
---
+++ OrientaçãoaObjetos/Interface/SegundoOO/Televisao.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SegundoOO
+{
+    internal class Televisao : IEletronico
+    {
+        private int _volts;
+        private int _potencia;
+        private bool _ligado;
+
+        public bool Ligado
+        {
+            get => _ligado;
+            set => _ligado = value;
+        }
+        public  void Ligar()
+        {
+            _ligado = true;
+            Console.WriteLine("Iniciando o equipamento");
+        }
+
+        public void Desligar()
+        {
+            _ligado = false;
+            Console.WriteLine("Desligando o equipamento");
+        }
+    }
+}
