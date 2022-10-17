ce6d2bd4ab45b26203dc1079eb6d2cb51080b643
Isaias de Jesus Barros <isaiasjbarross@gmail.com>
2022-10-17 10:19:21 -0400

    Exercicios lista dicionarios OOP 7/7

---
+++ Exercicios_OOP_Lista_Dicionarios/SetimoExercicio/Classes/Livro.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SetimoExercicio.Classes
+{
+    class Livro
+    {
+        private string _nomeLivro;
+        private int _qtdPaginas;
+        private Dictionary<string, int> livro = new Dictionary<string, int>();
+        public string NomeLivro
+        {
+            get => _nomeLivro; 
+            set { _nomeLivro = value; }
+        }
+        public int qtdPaginas{
+            get => _qtdPaginas;
+            set { _qtdPaginas = value; }
+        }
+
+        public bool adcionarLivros()
+        {
+            
+            if (!livro.Keys.Contains(_nomeLivro))
+            {
+                livro.Add(_nomeLivro, _qtdPaginas);
+                return true;
+            }
+            else
+            {
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("\n\tLivro já cadastrado\n");
+                Console.ResetColor();
+
+                return false;
+            }
+        }
+        public void ConsultarTodosOsLivrosCadastrados()
+        {
+            Program.LogoLivraria();
+
+            if (livro.Count > 0)
+            {
+                int count = 1;
+                foreach (KeyValuePair<string, int> livros in livro)
+                {
+                    Console.WriteLine("\n\t============= Livros =============\n"+"\t"+count++ +"º");
+                    Console.WriteLine("\tNome do livro: " + livros.Key);
+                    Console.WriteLine("\tQuantidade de páginas: " + livros.Value);
+                    Console.WriteLine("\t==================================\n");
+                }
+            }
+            else
+            {
+                Console.WriteLine("\tNão foram encontrados livros cadastrados");
+            }
+
+            Console.ReadKey();
+        }
+        public void ConsultarLivroPeloNome()
+        {
+           
+            Program.LogoLivraria();
+            
+            Console.Write("\tDigite o nome do livro que deseja encontrar: ");
+            string nomeLivro = Console.ReadLine();
+
+            var resultado = livro.FirstOrDefault((livro) => (livro.Key.Equals(nomeLivro)));
+            if (resultado.Key != null){
+
+                Console.WriteLine("\tNome: " + resultado.Key + "\n\tQuantidade de páginas: " + resultado.Value);
+            }
+            else{
+                Console.WriteLine("\tLivro Não encontrado");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
+    
---
+++ Exercicios_OOP_Lista_Dicionarios/SetimoExercicio/Program.cs
@@ -0,0 +1,110 @@
+﻿using SetimoExercicio.Classes;
+
+namespace SetimoExercicio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+              7) Crie um dicionário que faça o armazenamento de Livros. Os dados para serem armazenados serão
+                o nome do livro e a quantidade de páginas. Em um menu inicial, possibilite que o usuário do
+                sistema possa cadastrar, consultar todos livros cadastrados ou pesquisar pelo nome do livro.
+             
+             */
+            Livro livro = new Livro();
+            Menu(); 
+        }
+       public static void LogoLivraria()
+        {
+            Console.Clear();
+
+            Console.WriteLine("\t=============================");
+            Console.WriteLine("\t      Livraria Trindade");
+            Console.WriteLine("\t=============================\n");
+        }
+        static void Menu()
+        {
+            Livro livro = new Livro();
+            do
+            {
+                Console.Clear();
+
+                LogoLivraria();
+                Console.WriteLine("\t[1]-Cadastrar Livro\n\t[2]-Consultar todos o livros cadastrados\n\t[3]-Consultar pelo nome do livro\n\t[4]-Sair\n");
+                Console.Write("\tOpção: ");
+                string opc = Console.ReadLine();
+
+
+                switch (opc)
+                {   
+                    case "1":
+                        CadastrarLivro(ref livro);
+                        break;
+                    case "2":
+                        livro.ConsultarTodosOsLivrosCadastrados();
+                        break;
+                    case "3":
+                        livro.ConsultarLivroPeloNome();
+                        break;
+                    case "4":
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("\n\tOpção incorreta, pressione qualquer tecla para continuar");
+                        Console.ReadKey();
+                        break;
+                }
+
+
+            } while (true);
+        }
+        static void CadastrarLivro(ref Livro livro)
+        {
+           
+
+            while (true)
+            {
+                Console.Clear();
+                LogoLivraria();
+
+                Console.Write("\tNome do livro: ");
+                livro.NomeLivro = Console.ReadLine();
+
+                bool entrouNoCatch = false;
+                while(true)
+                {
+                    Console.Write("\tQuantidade de páginas: ");
+                    try
+                    {
+                        livro.qtdPaginas = int.Parse(Console.ReadLine());
+                    }
+                    catch
+                    {
+                        entrouNoCatch = true;
+                        Console.WriteLine("\tDigite um número de páginas... Pressione para continuar");
+                        Console.ReadKey();
+                        Console.Clear();
+                    }
+
+                    if(entrouNoCatch is true) { entrouNoCatch = false; continue; }
+                    else { break; }
+                }
+
+                livro.adcionarLivros();
+
+                CONTINUAR:
+                Console.Write("\n\tContinuar Cadastrando [1]-Sim/[2]-Não\n\tDigite: ");
+                string continuar = Console.ReadLine();
+                if (continuar != "2" && continuar != "1")
+                {
+                    goto CONTINUAR;
+                }
+                else if (continuar == "2") { break; }
+                else { }
+            }
+
+        }
+
+    }
+}
