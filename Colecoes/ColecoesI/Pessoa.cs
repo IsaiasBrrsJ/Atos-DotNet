bde4bb4250ce5fea9bc9d89e5e2530dc706a916f
Isaias de Jesus Barros <isaiasjbarross@gmail.com>
2022-11-24 11:16:16 -0400

    colecões

---
+++ Colecoes/ColecoesI/Pessoa.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColecoesI
+{
+    public class Pessoa :IComparable
+    {
+        public Pessoa(string nome, string cPF)
+        {
+            Nome = nome;
+
+            CPF = cPF;
+          
+        }
+
+        public string Nome { get; set; }
+
+        public string CPF { get; set; }
+       
+
+        //public int CompareTo(object? obj)
+        //{
+        //    return null;
+        //}
+
+        public override bool Equals(object? obj)
+        {
+            Console.WriteLine("CPF já cadastrado");
+            return obj is Pessoa pessoa &&
+                   CPF == pessoa.CPF;
+
+
+        }
+        public override string ToString()
+        {
+            return $"{Nome} {CPF}";
+        }
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(CPF);
+        }
+
+        public int CompareTo(object? obj)
+        {
+            Pessoa pessoa = obj as Pessoa;
+
+            if(this.Nome == pessoa.Nome)
+            {
+                return 0;
+            }
+            else if(this.Nome.CompareTo(pessoa.Nome) < 0)
+            {
+                return -1;
+            }
+
+            return 1;
+        }
+
+
+        //public override string ToString()
+        //{
+        //    Console.WriteLine("Veio aqui");
+        //    return $"Nome: {Nome} \nCPF: {CPF}\nDataNascimento: {dataNascimento}";
+        //}
+
+
+        //public int CompareTo(object? obj)
+        //{
+        //    Pessoa p = (Pessoa)obj;
+
+        //    if (this.Nome == p.Nome)
+        //    {
+        //        return 0;
+        //    }
+        //    else if (Nome.CompareTo(p.Nome) < 0)
+        //    {
+
+        //        return -1;
+        //    }
+
+        //    return 1;
+        //}
+
+    }
+}
---
+++ Colecoes/ColecoesI/Program.cs
@@ -0,0 +1,89 @@
+﻿namespace ColecoesI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region lista primitiva
+            //List<string> palavras = new List<string>();
+
+            //  for (int i = 0; i < 5; i++)
+            //  {
+            //      palavras.Add("nome "+i.ToString());
+            //  }
+
+            //  if (!palavras.Contains("nome2 "))
+            //  {
+            //      palavras.Add("nome2");
+            //  }
+
+            //  foreach (var item in palavras)
+            //  {
+            //      Console.WriteLine(item);
+            //}
+            #endregion
+
+
+            //List<Pessoa> pessoas = new List<Pessoa>();
+            //while (true)
+            //{
+            //    pessoas.Add(new Pessoa("Isaias", "333333", DateOnly.Parse("12-12-12")));
+
+            //   if(pessoas.Contains()
+            //    {
+
+            //    }
+            //}
+
+            //foreach (var item in pessoas)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //Pessoa p;
+            //SortedSet<Pessoa> pessoa = new SortedSet<Pessoa>();
+            //Random r = new Random();
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    var n = r.Next(10);
+            //    if (!pessoa.Add(new Pessoa("Nome" + n, "" + n, DateOnly.Parse("11/11/11"))))
+            //    {
+            //        Console.WriteLine("PEssoa já cadastrada");
+            //    }
+            //}
+
+            //foreach (var item in pessoa)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //HashSet<Pessoa> pessoa = new HashSet<Pessoa>();
+            //Random r = new Random();
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    var n = r.Next(10);
+
+            //    var a  = pessoa.Add(new Pessoa(n.ToString(),  n.ToString(), DateOnly.Parse("11/11/11")));
+
+            //}
+
+            //foreach (var item in pessoa)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            List<Pessoa> pe = new List<Pessoa>();
+            pe.Add(new Pessoa("Barros", "teste"));
+            pe.Add(new Pessoa("teste", "TETE"));
+            pe.Add(new Pessoa("Aline", "12"));
+            pe.Sort();
+
+            foreach (var item in pe)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
