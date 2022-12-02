71fe97b3ad272dccee3118e07056a26a0f800f57
Isaias de Jesus Barros <isaiasjbarross@gmail.com>
2022-12-02 10:37:25 -0400

    Aula API

---
+++ Api/PrimeiraAulaAPI/PrimeiraAulaAPI/Context.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PrimeiraAulaAPI.DataModel;
+
+namespace PrimeiraAulaAPI
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string configBanco = @"Data Source=localhost;initial Catalog=primeiraAulaApi;User ID=AtosEntity2;password=sed-09lim;language=Portuguese;Trusted_Connection=True;TrustServerCertificate=True;";
+
+            optionsBuilder.UseSqlServer(configBanco);
+
+
+        }
+
+        public DbSet<Pessoa> Pessoas { get; set; }
+    }
+}
--- Api/PrimeiraAulaAPI/PrimeiraAulaAPI/Controllers/ApiController.cs
+++ Api/PrimeiraAulaAPI/PrimeiraAulaAPI/Controllers/ApiController.cs
@@ -1,32 +1,132 @@
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PrimeiraAulaAPI.DataModel;
 
 namespace PrimeiraAulaAPI.Controllers
 {
     [ApiController]
-    [Route("a")]
+    [Route("api")]
     public class ApiController : ControllerBase
     {
-        [HttpGet("nome")]
-        public string RetornaNome()
+        #region aulaDeOntem
+        //[HttpGet("nome")]
+        //public string RetornaNome()
+        //{
+        //    return "Isaias Jb";
+        //}
+
+        //[HttpGet("idade")]
+        //public int RetornaIdade()
+        //{
+        //    return 0;
+        //}
+
+        //[HttpPut("nome/{nome}")]
+        //public string Nome([FromRoute] string nome)
+        //{
+        //    return nome;
+        //}
+
+        //[HttpPut("nome/{nome}/idade/{idade}")]
+        //public string NomeIdade([FromRoute] string nome, [FromRoute]int idade)
+        // => idade > 18 ? $"{nome} é maior de idade" : $"{nome} tem {idade}";
+        #endregion
+
+        [HttpGet]
+        [Route("pessoas")]
+        public async Task<IActionResult> getAllAsync([FromServices] Context contexto)
         {
-            return "Isaias Jb";
+            var pessoas = await contexto.Pessoas.AsNoTracking() // não rastreia apenas trás um copia do bd melhro desempenho, remocenda-se seu uso apenas em consultas
+                .ToListAsync();
+
+            return pessoas == null ? NotFound() : Ok(pessoas);
         }
 
-        [HttpGet("idade")]
-        public int RetornaIdade()
+        [HttpGet]
+        [Route("pessoas/{id}")]
+        public async Task<IActionResult> getAllAsync([FromServices] Context contexto, [FromRoute] int id)
         {
-            return 0;
+            var pessoas = await contexto.Pessoas.AsNoTracking() // não rastreia apenas trás um copia do bd melhro desempenho, remocenda-se seu uso apenas em consultas
+                .FirstOrDefaultAsync(p => p.Id == id);
+
+            return pessoas == null ? NotFound() : Ok(pessoas);
         }
 
-        [HttpPut("nome/{nome}")]
-        public string Nome([FromRoute] string nome)
+        [HttpPost]
+        [Route("pessoas")]
+        public async Task<IActionResult> PostAsync(
+            [FromServices] Context contexto,
+            [FromBody] Pessoa pessoa
+            )
         {
-            return nome;
+            try
+            {
+                await contexto.Pessoas.AddAsync(pessoa);
+                await contexto.SaveChangesAsync();
+                return Created($"api/pessoas/{pessoa.Id}", pessoa);
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
-        [HttpPut("nome/{nome}/idade/{idade}")]
-        public string NomeIdade([FromRoute] string nome, [FromRoute]int idade)
-         => idade > 18 ? $"{nome} é maior de idade" : $"{nome} tem {idade}";
+        [HttpPut]
+        [Route("pessoas/{id}")]
+        public async Task<IActionResult> PutAsync(
+            [FromServices] Context contexto, [FromBody] Pessoa pessoa, [FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest("Model Inválida");
+
+            var p = await contexto.Pessoas.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (p is null)
+                return NotFound("Pessoa não encontrada");
+    
+
+            try
+            {
+                p.Nome = pessoa.Nome;
+
+                contexto.Pessoas.Update(p);
+                await contexto.SaveChangesAsync();
+                return Ok(p);
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        [HttpDelete]
+        [Route("pessoas/{id}")]
+        public async Task<IActionResult> DeleteAsync(
+             [FromServices] Context contexto, [FromRoute] int id)
+        {
+            var p = await contexto.Pessoas.FirstOrDefaultAsync(x => x.Id==id);
+
+            if (p is null)
+                return BadRequest("Pessoa não encontrada");
+
+
+            try
+            {
+                contexto.Pessoas.Remove(p);
+                await contexto.SaveChangesAsync();
+                return Ok(p);
+
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+            
+
+
         
     }
+
+ 
 }
